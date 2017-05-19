fa2b2d82ff6ffe670db13b35bd34b318b443e75a
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-05-19 18:29:57 +0200

    Refactor: move getCommits ouside ifs

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ import { platform } from 'os';
 import makepath from 'path';
 import pathIsAbsolute from 'path-is-absolute';
 
-
 const cwd = process.cwd();
 
 const taggedGitCommits = ({ path, lookBehind, local, remote } = {}) => {
   let taggedCommits = [];
+  let getCommits;
 
   let thisPath = path || cwd;
   thisPath = pathIsAbsolute(thisPath) ? thisPath : makepath.join(cwd, thisPath);
   const thisLookBehind = lookBehind || 1;
   const thisLocal = local === undefined ? true : local;
   const thisRemote = remote || 'origin';
-
-  console.log('#####');
-  console.log(local);
-  console.log(thisLocal);
 
   if (!isGit(thisPath)) {
     return [];
@@ -29,8 +25,6 @@ const taggedGitCommits = ({ path, lookBehind, local, remote } = {}) => {
     let getCommitsExec;
 
     if (thisLocal) {
-      console.log('in');
-      console.log(thisLocal);
       if (platform() === 'win32') {
         getCommitsExec = `pushd ${thisPath} & git show-ref`;
         getTaggedCommitsExec = `pushd ${thisPath} & git show-ref --tags`;
@@ -39,17 +33,8 @@ const taggedGitCommits = ({ path, lookBehind, local, remote } = {}) => {
         getTaggedCommitsExec = `(cd ${thisPath} ; git show-ref --tags)`;
       }
 
-      const getCommits = execa.shellSync(getCommitsExec).stdout;
-
-      if (getCommits.includes('refs/tags')) {
-        const getTaggedCommits = execa.shellSync(getTaggedCommitsExec).stdout;
-
-        taggedCommits = getTaggedCommits.split('\n');
-        taggedCommits = taggedCommits.slice(-Math.abs(thisLookBehind));
-      }
+      getCommits = execa.shellSync(getCommitsExec).stdout;
     } else {
-      console.log('out');
-      console.log(thisLocal);
       if (platform() === 'win32') {
         getCommitsExec = `pushd ${thisPath} & git ls-remote ${thisRemote}`;
         getTaggedCommitsExec = `pushd ${thisPath} & git ls-remote --tags ${thisRemote}`;
@@ -58,14 +43,14 @@ const taggedGitCommits = ({ path, lookBehind, local, remote } = {}) => {
         getTaggedCommitsExec = `(cd ${thisPath} ; git ls-remote --tags ${thisRemote})`;
       }
 
-      const getCommits = execa.shellSync(getCommitsExec).stdout;
+      getCommits = execa.shellSync(getCommitsExec).stdout;
+    }
 
-      if (getCommits.includes('refs/tags')) {
-        const getTaggedCommits = execa.shellSync(getTaggedCommitsExec).stdout;
+    if (getCommits.includes('refs/tags')) {
+      const getTaggedCommits = execa.shellSync(getTaggedCommitsExec).stdout;
 
-        taggedCommits = getTaggedCommits.split('\n');
-        taggedCommits = taggedCommits.slice(-Math.abs(thisLookBehind));
-      }
+      taggedCommits = getTaggedCommits.split('\n');
+      taggedCommits = taggedCommits.slice(-Math.abs(thisLookBehind));
     }
     return taggedCommits;
   } catch (e) {
